1cf4b4df380fb977472e1ef266a4b3b27755736b
Nia Abrasheva <nabrasheva01@gmail.com>
2023-06-15 17:33:41 +0300

    added project view and functionality

--- src/server/src/controllers/projects-controller.ts
+++ src/server/src/controllers/projects-controller.ts
@@ -1,6 +1,10 @@
 import { IProject} from "../interfaces/projects";
 import {parseProjectDtoToIProject} from "../utils/projects-utils";
+import {ITicket} from "../interfaces/tickets";
+import {parseTaskDtoToITicket} from "../utils/tickets-utils";
 
+import {ProjectDto} from '../database/dtos/ProjectDto';
+const CreateProjectDto = require('../database/dtos/create/CreateProjectDto')
 const ProjectRepository = require('../database/repositories/ProjectRepository');
 
 export class ProjectController {
@@ -37,5 +41,20 @@ export class ProjectController {
 
         return parseProjectDtoToIProject(foundProject);
     }
+
+    public async addProject (project: IProject): Promise<IProject | undefined> {
+        console.log(project);
+
+        const newProject = new CreateProjectDto(project.projectKey, project.name, project.description, project.creationDate, project.leadUserId);
+        console.log(newProject);
+        const createTicket: ProjectDto = await ProjectRepository.createProject(newProject);
+        console.log("Successfully created ticket" + createTicket);
+        return parseProjectDtoToIProject(createTicket);
+    };
+
+    public async deleteProjectByProjectKey(project_num: string): Promise<void> {
+        const deleteProject:ProjectDto = await ProjectRepository.deleteProjectById(project_num);
+        console.log("Delete project is: \n" + deleteProject);
+    };
 
 }
