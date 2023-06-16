6a8235351ed1673bbeb1f969a7bd1eb832a66a78
Veronika Racheva <veronika.racheva01@gmail.com>
2023-06-16 20:04:28 +0300

    refactor

--- src/server/src/database/repositories/UserRepository.js
+++ src/server/src/database/repositories/UserRepository.js
@@ -23,46 +23,11 @@ class UserRepository {
             VALUES ($1, $2, $3)
             RETURNING *
         `;
-
         const values = [user.name, user.email, user.hashedPassword];
         const {rows} = await db.pool.query(query, values);
-        return new UserDto(rows[0].id, rows[0].name, rows[0].email, rows[0].hashed_password);
+
+        return new UserDto(rows[0].id, rows[0].name, rows[0].email);
     }
-    
-    // //register
-    // async checkIfUserExists(username) {
-    //     const query = `
-    //         SELECT *
-    //         FROM users
-    //         WHERE name = $1
-    //     `;
-    //     const values = [username];
-    //     const {rows} = await db.pool.query(query, values);
-
-    //     return rows.length > 0 ? true : false;
-    // }
-    
-    //login
-    // async validateUser(username,password){
-    //     const query = `
-    //         SELECT *
-    //         FROM users
-    //         WHERE name = $1 AND
-    //         hashed_password = $2
-    //     `;
-
-    //     let hashedPassword= "";
-    //     bcrypt.genSalt(15, function(err, salt) {
-    //         bcrypt.hash(password, salt, function(err, hash) {
-    //             hashedPassword = hash;
-    //         });
-    //     });
-
-    //     const values = [username,hashedPassword];
-    //     const {rows} = await db.pool.query(query, values);
-
-    //     return rows.length > 0 ? true : false;
-    // }
 
     async getUserById(id) {
         const query = `
@@ -73,7 +38,7 @@ class UserRepository {
         const values = [id];
         const {rows} = await db.pool.query(query, values);
 
-        return new UserDto(rows[0].id, rows[0].name, rows[0].email, rows[0].hashed_password);
+        return new UserDto(rows[0].id, rows[0].name, rows[0].email);
     }
 
     async updateUserById(id, user) {
@@ -88,7 +53,7 @@ class UserRepository {
         const values = [user.name, user.email, user.hashedPassword, id];
         const {rows} = await db.pool.query(query, values);
 
-        return new UserDto(rows[0].id, rows[0].name, rows[0].email, rows[0].hashed_password);
+        return new UserDto(rows[0].id, rows[0].name, rows[0].email);
     }
 
     async deleteUserById(id) {
@@ -100,8 +65,8 @@ class UserRepository {
         `;
         const values = [id];
         const {rows} = await db.pool.query(query, values);
-        return new UserDto(rows[0].id, rows[0].name, rows[0].email, rows[0].hashed_password);
+        return new UserDto(rows[0].id, rows[0].name, rows[0].email);
     }
 }
 
-module.export = new UserRepository();
+module.exports = new UserRepository();
